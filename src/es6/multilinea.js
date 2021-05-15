edc13ee1fbc8e5cd30f9706e3dd48650312ad9b5
wild.chamo <joselitobedoya230@gmail.com>
2021-05-15 12:34:41 -0500

    desestructurización

--- src/es6/multilinea.js
+++ src/es6/multilinea.js
@@ -7,3 +7,18 @@ let lorem2 =`otra frase epica
 ahora es otra frase epica
 `
 console.log(lorem2);
+
+
+let person= {
+    'name':'Jose luis',
+    'age':19,
+    'country':'COL'
+}
+
+console.log(person.name,person.age, person.country);
+
+//ec6
+let{name,age} =person;
+
+
+console.log(name,age);
