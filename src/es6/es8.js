5ac246721c16616f690e561865f7f2a575388154
wild.chamo <joselitobedoya230@gmail.com>
2021-05-16 14:17:53 -0500

    async await

--- src/es6/es8.js
+++ src/es6/es8.js
@@ -29,3 +29,40 @@ const string ="hello";
 console.log(string.padStart(7,'hi'))
 console.log(string.padEnd(12,'------'))
 
+//Async await
+
+const helloWorld= ()=>{
+    return new Promise((resolve,reject)=>{
+        (true)
+        ? setTimeout (()=> resolve('hello world'),3000)
+        : reject (new Error('test error'))
+    })
+};
+
+
+const helloAsync = async () =>{
+    const hello= await helloWorld();
+    console.log(hello);
+}
+helloAsync();
+
+
+const helloWorld= ()=>{
+    return new Promise((resolve,reject)=>{
+        (true)// false para romper
+        ? setTimeout (()=> resolve('hello world'),3000)
+        : reject (new Error('test error'))
+    })
+};
+
+const anotherFunction = async () =>{
+    try{
+        const hello= await helloWorld();
+        console.log(hello);
+    }
+    catch(error){
+        console.log(error)
+    }
+};
+
+anotherFunction();
