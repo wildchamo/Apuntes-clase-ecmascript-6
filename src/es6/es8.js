ac029428002dd515e268c992f471fa41a18eca21
wild.chamo <joselitobedoya230@gmail.com>
2021-05-16 11:43:12 -0500

    object entries y pegar string sin concatenar

--- src/es6/es8.js
+++ src/es6/es8.js
@@ -12,3 +12,20 @@ const data= {
 const entries = Object.entries(data);
 console.log(entries);
 console.log(entries.length);
+
+const data={
+    frontend: 'Jose Lo',
+    backend: 'Santiago',
+    design:'Ana'
+}
+
+const values=Object.values(data)
+console.log(values);
+console.log(values.length);
+
+
+//padding?
+const string ="hello";
+console.log(string.padStart(7,'hi'))
+console.log(string.padEnd(12,'------'))
+
