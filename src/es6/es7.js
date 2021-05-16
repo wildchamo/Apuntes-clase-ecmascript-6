c42247fb490dd0108d77a7110544658ef3413629
wild.chamo <joselitobedoya230@gmail.com>
2021-05-16 11:31:06 -0500

    es7: includes y operaciones exponenciales

---
+++ src/es6/es7.js
@@ -0,0 +1,15 @@
+// includes y poperaciones exponenciales
+
+let numbers =[1,2,3,4,5,6,7];
+
+if (numbers.includes(9)){
+    console.log("si se encuentra el valor 9");
+}else{
+    console.log(" no se encuentra")
+}
+
+
+
+let base= 4;
+let exponente= 3;
+let resultado= base ** exponente;
