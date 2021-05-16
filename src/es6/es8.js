086fb34c02d8009bbdd978ac0da1eb580dab84d8
wild.chamo <joselitobedoya230@gmail.com>
2021-05-16 11:35:59 -0500

    object entries

---
+++ src/es6/es8.js
@@ -0,0 +1,14 @@
+//se agrego async awake
+
+
+//object entries
+
+const data= {
+    frontend: 'Jose Lo',
+    backend: 'Santiago',
+    design:'Ana'
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
