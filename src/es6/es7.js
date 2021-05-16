5c78280a306e563c47ff0a0d6a4661d4da3785fb
wild.chamo <joselitobedoya230@gmail.com>
2021-05-16 11:32:19 -0500

    faltaba el console .log para ser si funcionaba el codigo

--- src/es6/es7.js
+++ src/es6/es7.js
@@ -1,4 +1,4 @@
-// includes y poperaciones exponenciales
+// includes y operaciones exponenciales
 
 let numbers =[1,2,3,4,5,6,7];
 
@@ -13,3 +13,4 @@ if (numbers.includes(9)){
 let base= 4;
 let exponente= 3;
 let resultado= base ** exponente;
+console.log(resultado);
